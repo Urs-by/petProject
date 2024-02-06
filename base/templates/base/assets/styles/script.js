dfa66c41a777bfe8082b4a2e6335536e6ec2f148
Artur-by <artur.by@mail.ru>
2024-02-06 08:28:33 +0300

    added registration form and models

--- base/templates/base/assets/styles/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const burgerButton = document.querySelector('.burger-button');
-    const menu = document.querySelector('.menu');
-
-    burgerButton.addEventListener('click', function () {
-        burgerButton.classList.toggle('active');
-        menu.classList.toggle('active');
-    });
-});
